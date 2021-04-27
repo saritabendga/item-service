a43b865dc1562e1bda6af8d3019b25ec70f4d8cf
Sarita Bendga <Sarita.Bendga@ibm.com>
2021-04-27 09:49:02 +0530

    final2 commit

--- src/main/java/com/example/demo/ItemserviceApplication.java
+++ src/main/java/com/example/demo/ItemserviceApplication.java
@@ -8,24 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ItemserviceApplication implements CommandLineRunner {
-
-    private ItemRepository itemRepository;
-
-     @Autowired
-    public ItemserviceApplication(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
-    }
-
+public class ItemserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ItemserviceApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
 
-         itemRepository.save(new Item(101,"Item1",100,10));
-        itemRepository.save(new Item(102,"Item2",101,30));
-        itemRepository.save(new Item(103,"Item3",102,15));
-    }
+
 }
